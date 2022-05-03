23ffaa8a2a0a4e184f7cc4897704a9525c997020
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-05-03 15:08:34 +0200

    Step 1 | Create a Homepage

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 require('dotenv').config();
 
 const express = require('express');
-const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
 const app = express();
+const hbs = require("hbs");
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+  app.get("/", (req, res, next) => res.render("index"));
 app.use(express.static(__dirname + '/public'));
 
+
+// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+    
+   
+  
+  // Retrieve an access token
+   spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error)); 
 
 // Our routes go here:
-
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
